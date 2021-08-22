aba8ab52c4ab2f0121cf361df44c2108f03f3e2e
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-08-22 06:54:45 -0300

    Fix preload scene not being loaded properly

--- Assets/Scripts/Preload.cs
+++ Assets/Scripts/Preload.cs
@@ -6,5 +6,18 @@ public class Preload : MonoBehaviour
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        if (PreloadInitializer.selectedScene > 0)
+        {
+            SceneManager.LoadScene(PreloadInitializer.selectedScene);
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
---
+++ Assets/Scripts/PreloadInitializer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreloadInitializer {
+    public static int selectedScene = -2;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void InitLoadingScene() {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (sceneIndex == 0) return;
+
+        selectedScene = sceneIndex;
+        SceneManager.LoadScene(0);
+    }
+}
